ca21ac89c491734bb762ec2a4be59403bf809135
ahmeddo7a <ahmedholdo@gmail.com>
2023-06-24 23:32:22 -0700

    sort.h fix #1

--- sort.h
+++ sort.h
@@ -2,6 +2,13 @@
 #define SORT_H
 #include <stddef.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -9,6 +16,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
+/**
+ * enum bool_e - manual definition of booleans, in case C99 stdbool.h not
+ * available
+ * @false: logical 0
+ * @true: logical 1
+ */
 typedef enum bool_e
 {
 	false = 0,
