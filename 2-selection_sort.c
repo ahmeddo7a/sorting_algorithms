62a1954cb2adb6f0e4ca8819cdfe47f4e489f05b
ziad <ziadabdelaziz1995@gmail.com>
2023-06-27 14:33:24 +0300

    [REPLACE]: change selection sort file name and upload other

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,33 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers using a selction sort
- * algorithm
- * @array: array of integers to be sorted
- * @size: amount of elements in array
+ * selection_sort - sort array on integers
+ * Description: the implementation of selection sort
+ * @array: array of integers
+ * @size: size of the array
+ * Return: None
  */
 void selection_sort(int *array, size_t size)
 {
-	int i, j, min_j, temp, n = (int)size;
+	size_t i, j, min;
+	int temp;
 
-	if (!array || size < 2)
-		return;
-
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		min_j = i;
-		for (j = i + 1; j < n; j++)
+		min = i;
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_j])
-			{
-				min_j = j;
-			}
+			if (array[j] < array[min])
+				min = j;
 		}
-		if (min_j != i)
+
+		if (i != min)
 		{
 			temp = array[i];
-			array[i] = array[min_j];
-			array[min_j] = temp;
+			array[i] = array[min];
+			array[min] = temp;
 			print_array(array, size);
 		}
 	}
+
 }
